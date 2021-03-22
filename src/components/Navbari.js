60d202dd6a423e90fd0ceb9912ea7d347a00ba06
Igniteh <n0majo00@students.oamk.fi>
2021-03-22 14:31:21 +0200

    hae..

--- src/components/Navbari.js
+++ src/components/Navbari.js
@@ -16,7 +16,7 @@ return (
     </ul>
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Etsi tuotteita..." aria-label="Search" />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Hae</button>
+      <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Hae</button>
     </form>
   </div>
 </nav>
