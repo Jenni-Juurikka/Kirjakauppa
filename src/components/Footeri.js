be06a762d60e4b25367f8c17d7f61d8120c9f00b
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-03-23 12:53:17 +0200

    footerille responsiivisuutta

--- src/components/Footeri.js
+++ src/components/Footeri.js
@@ -5,11 +5,11 @@ function Footeri() {
     return (
         <footer className="pt-4 pt-md-5 border-top">
         <div className="row">
-          <div className="col-12 col-md">
+          <div className="col-sm-12 col-md-6 col-lg-3">
             <small className="d-block mb-3 text-muted">&copy; <a>Kirjakauppa</a> {new Date().getFullYear()}</small>
-            <img style={{width: 250, height: 250}} src={logo} />
+            <img style={{width: 200, height: 200}} src={logo} />
           </div>
-          <div className="col-6 col-md">
+          <div className="col-sm-12 col-md-6 col-lg-3">
             <h5>Aukiolo</h5>
             <ul className="list-unstyled text-small">
               <li>Maanantai 7.00 - 19.00</li>
@@ -21,7 +21,7 @@ function Footeri() {
               <li>Sunnutai 10.00 - 15.00</li>
             </ul>
           </div>
-          <div className="col-6 col-md">
+          <div className="col-sm-12 col-md-6 col-lg-3">
             <h5>Yhteystiedot</h5>
             <ul className="list-unstyled text-small">
               <li><a>kirjakauppa@gmail.com</a></li>
@@ -29,7 +29,7 @@ function Footeri() {
               <li><a>Oulu, Saaristonkatu 6-10</a></li>
             </ul>
           </div>
-          <div className="col-6 col-md">
+          <div className="col-sm-12 col-md-6 col-lg-3">
             <h5>Sosiaalinen media</h5>
             <ul className="list-unstyled text-small">
               <li><a className="link-secondary" href="https://www.facebook.com/">Facebook</a></li>
