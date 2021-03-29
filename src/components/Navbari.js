7d956db13fee3b8b651d663dbc7d257c36b0bc91
Igniteh <n0majo00@students.oamk.fi>
2021-03-29 10:35:14 +0300

    navbari

--- src/components/Navbari.js
+++ src/components/Navbari.js
@@ -2,76 +2,95 @@ import "../styles/colors.css";
 import logo1 from "../images/kirjakauppa.png";
 import { useState, useEffect, Link } from "react";
 
-const URL = 'https://localhost/kirjakauppa/';
+const URL = "http://localhost/kirjakauppa/products/searchproducts.php";
 
-export default function Navbari({URL}) {
+export default function Navbari() {
   const [categories, setCategories] = useState([]);
 
-
-  useEffect(async () => {
-    try {
-      const response = await fetch(URL + 'products/getcategories.php');
-      const json = await response.json();
-      if (response.ok) {
-        setCategories(json);
-      } else {
-        alert(json.error);
-      }
-    } catch (error) {
-      alert('ei toimi');
-    }
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          setCategories(response);
+        },
+        (error) => {
+          alert(error);
+        }
+      );
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              {categories.map(tuoteryhma => (
-                <li key={tuoteryhma.trnro}>
-                  <Link
-                    className="dropdown-item"
-                    to={{
-                      pathname: '/',
-                      state: {
-                        id: tuoteryhma.trnro,
-                        name: tuoteryhma.trnimi
-                      }
-                    }}
-                  >{tuoteryhma.trnimi}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <ol>
+                  {categories.map((tuoteryhma) => (
+                    <li key={tuoteryhma.trnro}>
+                      <a href="http://www.google.com">{tuoteryhma.trnimi}</a>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
   );
 }
