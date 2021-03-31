41dc08f7b7adba37227c6bd9619b3b8cf3ec140a
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-03-31 11:21:51 +0300

    navbaria muokattu

--- src/components/Navbaruusi.js
+++ src/components/Navbaruusi.js
@@ -23,34 +23,41 @@ export default function Navbaruusi({url, cart, setCategory}) {
     }, []);
 
     return (
-        <div>
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id=""></a>
-                    <ul className="dropdown-menu" aria-aria-labelledby="dropdown">
-                        {categories.map(tuoteryhma => (
-                            <li key={tuoteryhma.id}>
-                                <Link 
-                                    className="dropdown-item"
-                                    to={{
-                                        pathname: '/',
-                                        state: {
-                                        id: tuoteryhma.id,
-                                        name: tuoteryhma.name
-                                        }
-                                    }}
-                                    >{tuoteryhma.trnimi}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </li>
-            </ul>
-            <ul className="navbar-nav-nav ml-auto">
-                <li className="nav-item">
-                    <Cart cart={cart} />
-                </li>
-            </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Valikko</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Tuoteryhmät </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            {categories.map(tuoteryhma => (
+                                <li key={tuoteryhma.id}>
+                                    <Link 
+                                        className="dropdown-item"
+                                        to={{
+                                            pathname: '/',
+                                            state: {
+                                            id: tuoteryhma.id,
+                                            name: tuoteryhma.name
+                                            }
+                                        }}
+                                        >{tuoteryhma.trnimi}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                </ul>
+                <ul className="navbar-nav-nav ml-auto">
+                    <li className="nav-item">
+                        <Cart cart={cart} />
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
