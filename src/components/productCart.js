a4d7c032d47b685c7455d6b5d1cd1c95593d9e64
Jenni Juurikka <c0juje01@students.oamk.fi>
2021-04-12 10:41:33 +0300

    Ostoskoria

--- src/components/productCart.js
+++ src/components/productCart.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Header from './header';
 import Navbaruusi from './Navbaruusi';
 import Footeri from './Footeri';
+import addToCart from '../App';
 import '../App.css';
 
 export default function productCart(props) {
-    const {cart} = props;
     return (
         <div className="container">
             <div className="bg-color">
                 <Navbaruusi/>
                 <Header/>
                 <div className="container-fluid">
-                    <h3>Tuotteet</h3>
+                    <h3>Ostoskori</h3>
                     <div className="row">
-                        {cart.length === 0 && <div>Kori on tyhjä</div>}
+                        {addToCart(props).length === 0 && <div>Kori on tyhjä</div>}
                     </div>
-                    
+                    <div className="row">
+                        {addToCart(props)}
+                    </div>
                 </div>
             </div>
             <Footeri/>
