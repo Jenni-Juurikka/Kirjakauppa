7605652e6cf9bca7bea4eaef116595098f4fe628
Valeria Velichko <n0veva00@students.oamk.fi>
2021-04-15 12:19:02 +0300

    en kyllä tiiä mut pusken ko meni koko sydeemi solmuun

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ export default function Home({url, tuoteryhma, search, addToCart}) {
     const URLI = "http://localhost/kirjakauppa/img/img_";
     const [products, setProducts] = useState([]);
 
-
+    // hae valitun tuoteryhmän tuotteet
     useEffect(async() => {
         if (tuoteryhma !== null /*|| search !== ''*/) {
             let address = '';
@@ -40,25 +40,14 @@ export default function Home({url, tuoteryhma, search, addToCart}) {
             <h3>{tuoteryhma?.name}</h3>
             <div className="row">
                 {products.map(tuote => (
-                    <div className="col-6 col-sm-6 col-md-4 col-lg-3" key={tuote.id}>
-                        <Link 
-                            id="tuote" 
-                            to={{
-                                pathname: '/tuotesivu',
-                                state: {
-                                    id: tuote.id,
-                                    name: tuote.name
-                                }
-                            }
-                            }>
-                                <div className="perkele">
-                                <div className="book">
-                            <h6>{tuote.name}</h6></div>
+                    <div className="col-6 col-md-4 col-lg-3 col-xl-2" key={tuote.id}>
+                        <Link id="tuote" to="/tuotesivu">
+                        <h6>{tuote.name}</h6>
                             <div className="book_img img-fluid">
-                            <img src={URLI + tuote.id + ".png"} className="img-fluid"/>
+                                <img src={URLI + tuote.id + ".png"} className="img-fluid"/>
                             </div>
                             <p>{tuote.author}</p>
-                            <p>{tuote.price + "€"}</p></div>
+                            <p>{tuote.price}</p>
                         </Link>
                         <button className="btn btn-primary" type="button" onClick={e => addToCart(tuote)}>
                             Lisää ostoskoriin
