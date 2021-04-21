57165bdfd4b4d54a77ccd4b161ce701235fe8b0f
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-04-21 14:13:27 +0300

    ylläpito tuotteille

--- src/Yllapito.js
+++ src/Yllapito.js
@@ -4,11 +4,17 @@ import {Link} from 'react-router-dom';
 const YP_URL = 'http://localhost/kirjakauppa/yllapito/';
 export default function Yllapito() {
 
-    
+    const [tuoteryhmat, setTuoteryhmat] = useState([]);
     const [trname, setTrname] = useState('');
-    const[tuoteryhmat, setTuoteryhmat] = useState([]);
+    const [tuotteet , setTuotteet] = useState([]);
+    const [tname , setTname] = useState('');
+    const [author , setAuthor] = useState('');
+    const [price , setPrice] = useState('');
+    //const [image , setImage] = useState();
+    const [category_id , setCategory_id] = useState('');
     //const [ , set] = useState();
 
+    // tuoteryhmät
     useEffect(() => {
         let status = 0;
         fetch(YP_URL + 'showtuoteryhmat.php')
@@ -89,17 +95,105 @@ export default function Yllapito() {
         )
     }
 
-    function updateTuoteryhma(id) {
+    // function updateTuoteryhma(id) {
+    //     let status = 0;
+    //     fetch(YP_URL + 'saveupdatetr.php', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             id: id,
+    //             name: trname
+    //         })
+    //     })
+    //     .then(res => {
+    //         status = parseInt(res.status);
+    //         return res.json();
+    //     })
+    //     .then(
+    //         (res) => {
+    //             if (status === 200) {
+    //                 setTuoteryhmat(res);
+    //               } else {
+    //                 alert(res.error);
+    //               }
+    //         }, (error) => {
+    //             alert(error);
+    //         }
+    //     )
+    // }
+
+    // tuotteet
+    useEffect(() => {
         let status = 0;
-        fetch(YP_URL + 'saveupdatetr.php', {
+        fetch(YP_URL + 'showtuotteet.php')
+        .then(res => {
+            status = parseInt(res.status);
+            return res.json()
+        })
+        .then(
+            (res) => {
+                if (status === 200) {
+                setTuotteet(res);
+                } else {
+                alert(res.error);
+                }
+            }, (error) => {
+                alert(error);
+            }
+        )
+    }, [])
+
+    function addTuote(t) {
+        t.preventDefault();
+        let status = 0;
+        fetch(YP_URL + 'savetuote.php', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                id: id,
-                name: trname
+                name: tname,
+                author: author,
+                price: price,
+                //image: image,
+                category_id: category_id
+            })
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(
+            (res) => {
+                if (status === 200) {
+                    setTuotteet(tuotteet => [...tuotteet, res]);
+                    setTname('');
+                    setAuthor('');
+                    setPrice('');
+                    //setImage('');
+                    setCategory_id('');
+                } else {
+                    alert(res.error);
+                }
+            }, (error) => {
+                alert(error);
+            }
+        )
+    }
+
+    function deleteTuote(id) {
+        let status = 0;
+        fetch(YP_URL + 'deletetuote.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: id
             })
         })
         .then(res => {
@@ -109,7 +203,8 @@ export default function Yllapito() {
         .then(
             (res) => {
                 if (status === 200) {
-                    setTuoteryhmat(res);
+                    const newListWithoutRemoved = tuotteet.filter((tuote) => tuote.id !== id);
+                    setTuotteet(newListWithoutRemoved);
                   } else {
                     alert(res.error);
                   }
@@ -119,6 +214,7 @@ export default function Yllapito() {
         )
     }
 
+
     return (
         <div>
             <h4>Tuoteryhmät</h4>
@@ -127,6 +223,8 @@ export default function Yllapito() {
                     <div>
                         <label>Tuoteryhmän nimi</label>
                         <input value={trname} onChange={tr => setTrname(tr.target.value)}/>
+                    </div>
+                    <div>
                         <button>Lisää uusi tuoteryhmä</button>
                     </div>
                 </form>
@@ -139,7 +237,53 @@ export default function Yllapito() {
                                 <td>{tuoteryhma.id}</td>
                                 <td>{tuoteryhma.name}</td>
                                 <td><a className="delete" onClick={() => deleteTuoteryhma(tuoteryhma.id)} href="#">Poista</a></td>
-                                <td><a className="update" onClick={() => updateTuoteryhma(tuoteryhma.id)} href="#">Muokkaa</a></td>
+                                {/* <td><a className="update" onClick={() => updateTuoteryhma(tuoteryhma.id)} href="#">Muokkaa</a></td> */}
+                            </tr>
+                        </table>
+                    ))}
+                </ol>
+            </div>
+            <h4>Tuotteet</h4>
+            <div>
+                <form onSubmit={addTuote}>
+                    <div>
+                        <label>Kirjan nimi</label>
+                        <input value={tname} onChange={t => setTname(t.target.value)}/>
+                    </div>
+                    <div>
+                        <label>Kirjailija</label>
+                        <input value={author} onChange={t => setAuthor(t.target.value)}/>
+                    </div>
+                    <div>
+                        <label>Hinta</label>
+                        <input value={price} onChange={t => setPrice(t.target.value)}/>
+                    </div>
+                    <div>
+                        {/* <label>Kuva</label>
+                        <input value={tname} onChange={t => setImage(t.target.value)}/> */}
+                    </div>
+                    <div>
+                        <label>Kategoria</label>
+                        <input value={category_id} onChange={t => setCategory_id(t.target.value)}/>
+                    </div>
+                    <div>
+                        <button>Lisää uusi tuote</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <ol>
+                    {tuotteet.map(tuote => (
+                        <table className="table" key={tuote.id}>
+                            <tr>
+                                <td>{tuote.id}</td>
+                                <td>{tuote.name}</td>
+                                <td>{tuote.author}</td>
+                                <td>{tuote.price}</td>
+                                <td>{tuote.image}</td>
+                                <td>{tuote.category_id}</td>
+                                <td><a className="delete" onClick={() => deleteTuote(tuote.id)} href="#">Poista</a></td>
+                                {/* <td><a className="update" onClick={() => updateTuote(tuote.id)} href="#">Muokkaa</a></td> */}
                             </tr>
                         </table>
                     ))}
