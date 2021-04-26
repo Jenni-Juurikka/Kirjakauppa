429d0dea3456b29494f2ec85eecb92317c9675f4
unknown <n0majo00@students.oamk.fi>
2021-04-26 11:37:08 +0300

    navbaria ei tarvitse tuplaklikata

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
